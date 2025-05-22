8252b4e3e38b4fced469b9bc1307702d42f9b547
Luís Fernando Garcia da Cunha <luisfernandogarciadacunha@gmail.com>
2025-05-22 15:50:44 -0300

    troca de arquivo

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
-const hour = document.querySelector(".hour");
-const minute = document.querySelector(".minute");
-const second = document.querySelector(".second");
+const inputEl = document.getElementById("input");
+const infoTextEl = document.getElementById("info-text");
+const meaningContainerEl = document.getElementById("meaning-container");
+const titleEl = document.getElementById("title");
+const meaningEl = document.getElementById("meaning");
+const audioEl = document.getElementById("audio");
 
-function setDate() {
-  const now = new Date();
+async function fetchAPI(word) {
+  try {
+    infoTextEl.style.display = "block";
+    meaningContainerEl.style.display = "none";
+    infoTextEl.innerText = `Searching the meaning of "${word}"`;
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    const result = await fetch(url).then((res) => res.json());
 
-  const getSecond = now.getSeconds();
-  const getMinute = now.getMinutes();
-  const getHour = now.getHours();
-
-  const secondDegree = (getSecond / 60) * 360;
-  const minuteDegree = (getMinute / 60) * 360;
-  const hourDegree = (getHour / 12) * 360;
-
-  second.style.transform = `rotate(${secondDegree}deg)`;
-  minute.style.transform = `rotate(${minuteDegree}deg)`;
-  hour.style.transform = `rotate(${hourDegree}deg)`;
+    if (result.title) {
+      meaningContainerEl.style.display = "block";
+      infoTextEl.style.display = "none";
+      titleEl.innerText = word;
+      meaningEl.innerText = "N/A";
+      audioEl.style.display = "none";
+    } else {
+      infoTextEl.style.display = "none";
+      meaningContainerEl.style.display = "block";
+      audioEl.style.display = "inline-flex";
+      titleEl.innerText = result[0].word;
+      meaningEl.innerText = result[0].meanings[0].definitions[0].definition;
+      audioEl.src = result[0].phonetics[0].audio;
+    }
+  } catch (error) {
+    console.log(error);
+    infoTextEl.innerText = `an error happened, try again later`;
+  }
 }
 
-setInterval(setDate, 1000);
+inputEl.addEventListener("keyup", (e) => {
+  if (e.target.value && e.key === "Enter") {
+    fetchAPI(e.target.value);
+  }
+});
